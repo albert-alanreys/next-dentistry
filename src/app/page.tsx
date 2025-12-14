4d861a4086177129860814efa54ccbe301a2a308
Albert Alan-Reys <albert.alanreys@gmail.com>
2025-12-14 16:15:32 +1000

    Add base styles, variables, fonts and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import styles from './page.module.css';
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <h1>Наши ценности</h1>
+    </div>
+  );
 }
